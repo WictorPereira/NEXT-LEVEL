5aa412820c0d23399e7f2300d44b5f7f121bd804
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-11-05 17:48:02 -0300

    ajustes na logica
    ajuste na logica da função curtir, onde foi alterado para o usuário curtir apenas uma vez e a img mudar a cor quando for curtida também

--- js/home.js
+++ js/home.js
@@ -1,9 +1,21 @@
 // funçao de curtir
+let liked = false; // Variável de controle
 
-function somarLike (){
+function somarLike() {
     var likeElement = document.getElementById('like');
     var likeCount = parseInt(likeElement.innerText);
-    likeCount += 1;
-    
-    likeElement.innerText = likeCount+'k';
+
+    if (!liked) {
+        // Incrementa o like e adiciona o 'k'
+        likeCount += 1;
+        likeElement.innerText = likeCount + 'k';
+        likeIcon.classList.add('liked'); // Adiciona a classe 'liked' para mudar a cor
+        liked = true; // Marca como "curtido"
+    } else {
+        // Volta ao valor original 
+        likeCount -= 1;
+        likeElement.innerText = likeCount + 'k';
+        likeIcon.classList.remove('liked'); //Remove
+        liked = false; // Marca como "não curtido"
+    }
 }
