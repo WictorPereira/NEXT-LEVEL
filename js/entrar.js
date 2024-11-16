807f9d38e33ddc1db9fa84f812ebad5ee262bd0c
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-11-16 14:48:08 -0300

    Ajuste login
    Logica que permite fazer login com Enter do teclado

--- js/entrar.js
+++ js/entrar.js
@@ -1,3 +1,12 @@
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+        logar(); // Chama a função logar
+    }
+});
+
+
+
 function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
